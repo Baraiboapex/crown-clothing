2b59e34c152a1d3ce5c9a8668315595e2f0b2bda
Matthew Bailey <MBailey@jtafla.com>
2020-06-05 19:21:36 -0400

    Removed the COLLECTIO_ID_MAP component from chop.selector.js since it was no longer needed along with the selectCollections selector on the collections-overview component.

--- src/components/collections-overview/collections-overview.component.jsx
+++ src/components/collections-overview/collections-overview.component.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import {connect} from 'react-redux';
 import {createStructuredSelector} from 'reselect'; 
 
-import {selectCollection, selectCollectionItems} from '../../redux/shop/shop.selector';
+import {selectCollectionItems} from '../../redux/shop/shop.selector';
 
 import CollectionPreview from '../collection-preview/collection-preview.component';
 
